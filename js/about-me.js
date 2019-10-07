d90fd9cec61eb78de35494977ea1cde39c796dd3
mai <mai.yusuf28@yahoo.com>
2019-10-07 15:42:27 +0300

    working in html , css and js

---
+++ js/about-me.js
@@ -0,0 +1,38 @@
+"use strict";
+var username=prompt("what is your name?");
+alert ("welcom"+ username);
+var age= prompt ("are you 24 years old ?");
+ age =age.toLowerCase();
+if (age=== "yes" || age === "y"){
+    alert ("it's my age" );
+} else if (age==="no"){
+    alert ( "maybe your older or younger than me");
+}
+var name = prompt ( " do you now that the meaning of mai is Naughty monkeys ?");
+name =name.toLowerCase();
+if ( name=== "yes" || name=== "y"){
+    alert ( "it's my name")
+}else if ( name==="no"){
+    alert ( "new information to know and it's my name");
+}
+var food= prompt (" do you feel hungry when you concentrate ?");
+food =food.toLowerCase();
+if ( food=== "yes" || food=== "y"){
+    alert("me too");
+}else if ( food==="no"){
+    alert ("you disagree with me");
+}
+var read= prompt (" do you like reading novel ?");
+read =read.toLowerCase();
+if ( read=== "yes" || read=== "y"){
+    alert ("reading will make you better i'm sure ");
+}else if ( read==="no"){
+    alert ("i think you must start reading");
+}
+var game=prompt ("do you like puzzle game ");
+game =game.toLowerCase();
+if ( game=== "yes" || game=== "y"){
+    alert("me too");
+}else if ( game==="no"){
+    alert ("try it, you will fell excited");
+}
